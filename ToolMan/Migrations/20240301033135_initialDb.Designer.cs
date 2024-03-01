a057d68eb7d067583d94c656e13d53f6047efcc4
blackWinds <745673576@qq.com>
2024-03-01 11:58:53 +0800

    add the migration file

--- ToolMan/Migrations/20240301033135_initialDb.Designer.cs
+++ ToolMan/Migrations/20240301033135_initialDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToolMan.Data;
 using Volo.Abp.EntityFrameworkCore;
@@ -11,9 +12,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ToolMan.Migrations
 {
     [DbContext(typeof(ToolManDbContext))]
-    partial class ToolManDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240301033135_initialDb")]
+    partial class initialDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
